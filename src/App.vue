3c769c3cceffe63f248bc8ed519cbc7a9cb7610d
volga52 <volga52@bk.ru>
2021-06-01 16:17:49 +0300

    improvement

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,198 @@
 <template>
-  <div id="app">
-    <Eshop />
+  <div id="app" class="wrapper">
+    <!-- <Eshop /> -->
+    <header class="header-block">
+      <div class="hearder-title">
+        <h1>Internet Shop</h1>
+      </div>
+      <div class="menu-top">
+        <form action="#" class="search">
+          <input
+            class="search-input"
+            type="search"
+            name="q"
+            placeholder="Поиск"
+          />
+          <button class="search-button" type="button">Найти</button>
+          <!-- <input class="search-button" type="submit" value="Найти"></p> -->
+        </form>
+
+        <button class="cart-button" type="button">Корзина</button>
+
+        <!-- <a href="#"><img src="img/shopping-cart1.png" alt="contacts"></a> -->
+      </div>
+    </header>
+
+    <div class="content">
+      <div class="container">
+        <div class="catalog-block">
+          <div class="catalog-title">Каталог</div>
+          <div class="catalog di-flex">
+            <div
+              class="good item-catalog"
+              v-for="item in goodList"
+              :key="item.id_product"
+            >
+              <img
+                src="#"
+                alt="Photo"
+                height="120"
+                winght="120"
+                class="image"
+              />
+              <div>
+                <div><b>Наименование</b>: {{ item.product_name }}</div>
+                <div><b>Цена за штуку</b>: {{ item.price }}</div>
+                <button
+                  class="addToBasket-btn"
+                  data-id_product="${this.good.id_product}"
+                >
+                  Добавить в корзину
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="cart-block">
+          <div class="cart-title">Корзина</div>
+          <div class="cart-list di-flex"></div>
+          <button class="cart-btn">очистить корзину</button>
+        </div>
+      </div>
+    </div>
+
+    <footer class="footer">
+      <hr />
+      <div class="notice">
+        <p>JavaScript Groop</p>
+        <p>2021 All rights reserved &copy;</p>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import Eshop from './components/Eshop'
+const API_URL =
+  "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
+
+// import Eshop from './components/Eshop'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Eshop,
-  }
-}
+    // Eshop,
+  },
+  data: () => ({
+    good: [],
+    goodList: [],
+  }),
+  mounted() {
+    this.makeGETRequest(`${API_URL}/catalogData.json`);
+  },
+  methods: {
+    makeGETRequest(url) {
+      fetch(url)
+        .then((data) => data.json())
+        .then((data) => {
+          this.good = data;
+          this.goodList = data;
+        });
+    },
+  },
+};
 </script>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.wrapper {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  max-width: 1400px;
+  margin: auto;
+}
+
+.content {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  margin: 0 16px;
+}
+
+.header-block {
+  background-color: beige;
+  /* margin: 0 auto;
+    width: 100vh; */
+}
+
+.hearder-title {
+  margin: 0 16px;
+}
+
+.menu-top {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 50px;
+}
+
+.search-button {
+  font-size: 16px;
+  margin: 5px;
+}
+
+.good {
+  border: 1px solid #000;
+  margin-bottom: 5px;
+  background: whitesmoke;
+}
+
+.item-catalog {
+  margin: 0 2px 5px;
+  flex: auto;
+  max-width: 200px;
+  min-width: 120px;
+  display: flex;
+  flex-direction: column;
+}
+
+.cart-list {
+  flex-direction: column;
+}
+
+.container {
+  max-width: 1140px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
+
+.di-flex {
+  display: flex;
+  justify-content: start;
+  flex-wrap: wrap;
+}
+
+footer {
+  /* color: black; */
+  background-color: rgb(199, 170, 131);
+  margin-top: 46px;
+}
+
+/* .footer {
+    margin: 0 auto;
+    align-self: center;
+    width: 100vh;
 } */
 
+.notice {
+  margin: 24px 24px 0 24px;
+  padding-bottom: 24px;
+  display: flex;
+  justify-content: space-between;
+}
 </style>
